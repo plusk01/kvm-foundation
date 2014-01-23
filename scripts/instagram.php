84f8b3a634096722f2514d66b5ab2fee229842d7
Parker Lusk <parkerclusk@gmail.com>
2014-01-22 23:22:26 -0700

    worked on index

---
+++ scripts/instagram.php
@@ -0,0 +1,16 @@
+<?php
+      function fetchData($url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+        $result = curl_exec($ch);
+        curl_close($ch); 
+        return $result;
+      }
+      $result = fetchData("https://api.instagram.com/v1/users/ID-GOES-HERE/media/recent/?access_token=TOKEN-GOES-HERE");
+      $result = json_decode($result);
+      foreach ($result->data as $post) {
+        // Do something with this data.
+      }
+    ?> 
